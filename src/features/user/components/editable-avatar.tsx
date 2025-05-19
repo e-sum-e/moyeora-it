62aaa58dffc7767d1c0760f5f20569bdc32621d8
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-19 15:17:44 +0900

    feat: 수정 가능한 아바타 컴포넌트 추가

---
+++ src/features/user/components/editable-avatar.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import { useRef, useState } from "react";
+import { toast } from "sonner";
+import { Avatar } from "@/components/avatar/index";
+import { isValidImageFile } from "@/features/user/utils/isValidImageFile";
+
+type EditableAvatarProps = {
+  imageSrc: string;
+  fallback: string;
+};
+
+/**
+ * 수정 가능한 아바타 컴포넌트
+ * 
+ * @param imageSrc 이미지 주소(문자열)
+ * @param fallback 아바타 폴백 문자열(문자열)
+ * @returns 수정 가능한 아바타 컴포넌트
+ */
+export const EditableAvatar = ({ imageSrc, fallback }: EditableAvatarProps) => {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const [currentImageSrc, setCurrentImageSrc] = useState(imageSrc);
+
+  /**
+   * 파일이 변경될 때 실행되는 함수
+   * 
+   * 유효한 이미지 파일인 경우, 미리보기 이미지 주소를 업데이트하고, 그렇지 않은 경우, 토스트 메시지를 표시.
+   * 
+   * @param e 파일 변경 이벤트
+   */
+  const fileChangeHandler: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    const file = e.target.files?.[0];
+    if (file && isValidImageFile(file)) {
+      setCurrentImageSrc(URL.createObjectURL(file));
+    } else {
+      toast.error("유효하지 않은 이미지 파일입니다.");
+    }
+    if (fileInputRef.current) {
+      fileInputRef.current.value = "";
+    }
+  };
+
+  return (
+    <div className={"relative"}>
+      <Avatar
+        imageSrc={currentImageSrc}
+        fallback={fallback}
+        className={"size-16"}
+        onClick={() => fileInputRef.current?.click()}
+      />
+      <input
+        ref={fileInputRef}
+        type={"file"}
+        accept={"image/*"}
+        onChange={fileChangeHandler}
+        className={"absolute inset-0 w-full h-full hidden"}
+        multiple={false}
+      />
+      <button
+        type={"button"}
+        onClick={() => {
+          setCurrentImageSrc(
+            "https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI="
+          );
+        }}
+      >
+        기본 이미지
+      </button>
+    </div>
+  );
+};
