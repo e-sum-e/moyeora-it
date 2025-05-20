75709fb7db5c468f661f8aee62cd8b21e485af46
kkukileon305 <kkukileon305@gmail.com>
2025-05-20 09:42:02 +0900

    feat: InputText를 텍스트관련 입력으로 타입을 제한, placeholder의 경우 placeholder="비밀번호"는 props로 받는 placeholder가 겹쳐져서 의미가 불필요하여 삭제

--- src/components/atoms/input-text/index.tsx
+++ src/components/atoms/input-text/index.tsx
@@ -1,17 +1,17 @@
 import { Input } from "@/components/ui/input";
 import * as React from "react";
 
+// 키보드로 직접 입력하는 type으로 제한
+type InputTextProps = React.ComponentProps<"input"> & {
+  type?: "text" | "password" | "search" | "email" | "url" | "tel" | "number";
+};
+
+// 사용자가 키보드로 직접 텍스트를 입력할 수 있는 Input입니다
+// type을 입력하지 않을 경우 기본 타입은 text입니다
 export const InputText = ({
   className,
-  type,
+  type = "text",
   ...props
-}: React.ComponentProps<"input">) => {
-  return (
-    <Input
-      placeholder="비밀번호"
-      className={`${className}`}
-      type={type}
-      {...props}
-    />
-  );
+}: InputTextProps) => {
+  return <Input className={`${className} text-black`} type={type} {...props} />;
 };
