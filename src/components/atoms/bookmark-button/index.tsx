66bcf5e821defe044c3d576dbcbcf5103399062e
Lee Yuna <leeyuna7689@gmail.com>
2025-05-20 14:43:10 +0900

    chore: .prettierrc에서 useTabs 옵션 true -> false 변경

--- src/components/atoms/bookmark-button/index.tsx
+++ src/components/atoms/bookmark-button/index.tsx
@@ -4,32 +4,32 @@ import { addBookmarkItem, removeBookmarkItem } from '@/features/bookmark';
 import { useState } from 'react';
 
 type BookmarkButtonProps = {
-	id: number;
-	isBookmark: boolean;
+  id: number;
+  isBookmark: boolean;
 };
 
 export const BookmarkButton = ({
-	id,
-	isBookmark: initialIsBookmarkState,
+  id,
+  isBookmark: initialIsBookmarkState,
 }: BookmarkButtonProps) => {
-	const [isBookmark, setIsBookmark] = useState<boolean>(initialIsBookmarkState);
+  const [isBookmark, setIsBookmark] = useState<boolean>(initialIsBookmarkState);
 
-	const bookmarkButtonToggleHandler = () => {
-		if (isBookmark) {
-			setIsBookmark(false);
-			removeBookmarkItem(id);
-		} else {
-			setIsBookmark(true);
-			addBookmarkItem(id);
-		}
-	};
+  const bookmarkButtonToggleHandler = () => {
+    if (isBookmark) {
+      setIsBookmark(false);
+      removeBookmarkItem(id);
+    } else {
+      setIsBookmark(true);
+      addBookmarkItem(id);
+    }
+  };
 
-	return (
-		<button
-			onClick={bookmarkButtonToggleHandler}
-			className={`${isBookmark ? 'bg-red-400' : ''}`}
-		>
-			찜하기
-		</button>
-	);
+  return (
+    <button
+      onClick={bookmarkButtonToggleHandler}
+      className={`${isBookmark ? 'bg-red-400' : ''}`}
+    >
+      찜하기
+    </button>
+  );
 };
