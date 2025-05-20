6d8b8a16f80cf34c9b04c4ab5e4dc0eeda8d8278
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-20 13:30:40 +0900

    feat: 이미지 파일 유효성 검사 함수 개선
    - 함수 이름을 isValidImageFile에서 validateImageFile로 변경
    - 이미지 파일이 유효하지 않은 경우, 이유도 반환하도록 변경

--- src/features/user/utils/isValidImageFile.ts
+++ src/features/user/utils/isValidImageFile.ts
@@ -7,18 +7,20 @@
  * 
  * @param file 검사할 이미지 파일
  * @param maxFileSize 최대 파일 크기(기본값: 5MB)
- * @returns 위 조건을 만족하는 경우 true, 그렇지 않은 경우 false
+ * @returns 이미지 파일 유효성 검사 결과 객체
+ * - isValid: 유효한 이미지 파일인 경우 true, 그렇지 않은 경우 false
+ * - errorMessage: 유효하지 않은 경우 오류 메시지
 */
-export const isValidImageFile = (file: File, maxFileSize = 5 * 1024 * 1024) => {
+export const validateImageFile = (file: File, maxFileSize = 5 * 1024 * 1024): { isValid: boolean; errorMessage?: string} => {
   if (!file.type.startsWith("image/")) {
-    return false;
+    return { isValid: false, errorMessage: '이미지 파일만 업로드할 수 있어요.'};
   }
 
   const fileSize = file.size;
 
   if (fileSize > maxFileSize) {
-    return false;
+    return { isValid: false, errorMessage: `파일 크기는 ${maxFileSize / 1024 / 1024}MB를 초과할 수 없어요.`};
   }
 
-  return true;
+  return { isValid: true };
 };
