a8ec5b1d9c0bbdca06d3eba9067b09a0a9b84ba3
seomi <seomi.lee@gmail.com>
2025-05-21 14:16:08 +0900

    feat: request 추가

---
+++ src/api/request.ts
@@ -0,0 +1,65 @@
+const baseUrl = `https://moyeora-it/api`; // 임시로 만든 url
+
+export const request = {
+  get: async (url: string, queryParams?: Record<string, string | number>) => {
+    const queryString = queryParams
+      ? '?' +
+        Object.entries(queryParams)
+          .map(([key, value]) => `${key}=${value}`)
+          .join('&')
+      : ''; // ?sort=deadline&order=asc
+
+    const response = await fetch(`${baseUrl}/${url}${queryString}`);
+
+    if (!response.ok) {
+      throw new Error('Unexpected Error');
+    }
+    return response.json();
+  },
+
+  post: async (
+    url: string,
+    body: BodyInit,
+    id?: string,
+    isJson = true, // json,form-data의 header가 다르게 가므로 확인 필요
+  ) => {
+    const headers = isJson ? { 'Content-Type': 'application/json' } : undefined;
+
+    const response = await fetch(`${baseUrl}/${url}${id ? `/${id}` : ''}`, {
+      method: 'POST',
+      headers,
+      body: isJson ? JSON.stringify(body) : body,
+    });
+
+    if (!response.ok) {
+      throw new Error('Unexpected Error');
+    }
+    return response.json();
+  },
+
+  patch: async (url: string, id: string, body: object) => {
+    const response = await fetch(`${baseUrl}/${url}/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    });
+
+    if (!response.ok) {
+      throw new Error('Unexpected Error');
+    }
+    return response.json();
+  },
+
+  delete: async (url: string, id: string) => {
+    const response = await fetch(`${baseUrl}/${url}/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (!response.ok) {
+      throw new Error('Unexpected Error');
+    }
+    return response.json();
+  },
+};
