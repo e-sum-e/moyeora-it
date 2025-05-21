59ff31ea1430d9d764785baf73747df3d05a3f3c
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-21 16:30:43 +0900

    comment: useDebounce 훅에 대한 주석 추가

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,5 +1,13 @@
 import { useCallback, useEffect, useRef } from 'react';
 
+/**
+ * 디바운스 훅
+ * 
+ * @param callback 호출할 함수
+ * @param delay 지연 시간
+ * @returns 디바운스된 콜백 함수
+ */
+
 export const useDebounce = <T extends (...args: Parameters<T>) => void>(
   callback: T,
   delay: number,
