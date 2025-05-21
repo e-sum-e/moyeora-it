a6b84dabbc91dc160bff7cb8e0ba6e76d7e9258b
HYEONJEONG LEE <71309720+hjleee93@users.noreply.github.com>
2025-05-21 10:20:41 +0900

    Merge pull request #37 from moyeora-it/fix
    자동 PR: fix → dev

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,17 @@
 //전역 파일 타입 정리
 
 export type User = {
-  id: string;
-  name: string;
-  email: string;
-  profileImage: string;
+	id: string;
+	name: string;
+	email: string;
+	profileImage: string;
 };
 
 /** 모임 만들기 폼에 사용되는 데이터들의 타입 */
 export type WriteForm = {
-  title: string;
-  maxParticipants: number;
-  deadline: Date;
-  startDate: Date;
-  endDate: Date;
+	title: string;
+	maxParticipants: number;
+	deadline: Date;
+	startDate: Date;
+	endDate: Date;
 };
