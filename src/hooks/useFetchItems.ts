3f5f93364e3413265a85cb7b1c9a52f6622cfdd2
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-21 17:02:58 +0900

    refactor: 쿼리 키 변경 및 queryFn 메소드 수정

--- src/hooks/useFetchItems.ts
+++ src/hooks/useFetchItems.ts
@@ -1,77 +1,16 @@
-import { QueryFunction, useSuspenseInfiniteQuery } from "@tanstack/react-query";
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
+import { request } from '@/api/request';
 
-enum Position {
-  'PM',
-  'PL',
-  'AA',
-  'TA',
-  'DA',
-  'QA',
-  'FE',
-  'BE',
-  'FS',
-}
-
-enum Skill {
-  'Java',
-  'JavaScript',
-  'TypeScript',
-  'Python',
-  'HTML/CSS',
-  'React',
-  'Vue.js',
-  'Kotlin',
-  'Spring',
-}
-
-type Group = {
-  id: number;
-  title: string;
-  deadline: Date;
-  endDate: Date;
-  maxParticipants: number;
-  description: string;
-  position: (Position | (string & {}))[];
-  skill: (Skill | (string & {}))[];
-  createdAt: Date;
-  isBookmark: boolean;
-  autoAllow: boolean;
-};
-
-type Response = {
-  items: Group[];
-  cursor: number;
-  hasNext: boolean;
-};
-
-type QueryParams = {
-  [key: string]: string | number | boolean;
-};
-
-type QueryKey = [_1: string, _2: string, _3: QueryParams];
-
-const fetchItems: QueryFunction<Response, QueryKey, number> = async ({ pageParam = 0, queryKey }) => {
-  const [, type, params] = queryKey;
-
-  const queryString = Object.entries(params)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
-
-  const response = await fetch(`https://example.com/api/${type}?next-cursor=${pageParam}&${queryString}`);
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch list');
-  }
-
-  const data = await response.json();
-
-  return data;
-};
-
-export const useFetchItems = ({ type, params }: { type: string; params: QueryParams }) => {
+export const useFetchItems = ({
+  url,
+  queryParams,
+}: {
+  url: string;
+  queryParams: Record<string, string | number>;
+}) => {
   return useSuspenseInfiniteQuery({
-    queryKey: ['items', type, params],
-    queryFn: fetchItems,
+    queryKey: ['items', url, queryParams],
+    queryFn: () => request.get(url, queryParams),
     initialPageParam: 0,
     getNextPageParam(lastPage) {
       return lastPage.hasNext ? lastPage.cursor : null;
