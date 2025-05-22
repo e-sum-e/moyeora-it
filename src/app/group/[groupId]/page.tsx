6b20a64007f93fc42f9e5763b45019632430b969
Lee Yuna <leeyuna7689@gmail.com>
2025-05-22 18:05:30 +0900

    feat: 댓글 컴포넌트 추가

--- src/app/group/[groupId]/page.tsx
+++ src/app/group/[groupId]/page.tsx
@@ -1,11 +1,26 @@
 import { ReplyForm } from '@/components/atoms/reply/reply-form';
+import { Reply } from '@/components/organisms/reply';
 
 export default function GroupDetailPage() {
   return (
     <div>
       <div>{/* 마크다운으로 된 설명문 */}</div>
-      <ReplyForm />
-      <div>{/* 댓글 */}</div>
+      <section className="w-4/5 mx-auto flex flex-col gap-10">
+        <ReplyForm />
+        <div>
+          {/* 댓글 리스트*/}
+          <Reply
+            writer={{
+              nickname: '작성자',
+              profileImage: '',
+              userId: 'a1',
+            }}
+            content="댓글입니다."
+            createdAt="2025.05.21"
+            replyId={1}
+          />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/organisms/reply/index.tsx
@@ -0,0 +1,24 @@
+import { ReplyContent } from '@/components/atoms/reply/reply-content';
+import { ReplyHeader } from '@/components/molecules/reply/reply-header';
+import { ReplyThread } from '@/components/molecules/reply/reply-thread';
+
+type ReplyProps = {
+  content: string;
+  writer: {
+    userId: string;
+    nickname: string;
+    profileImage: string;
+  };
+  createdAt: string;
+  replyId: number;
+};
+
+export const Reply = ({ content, writer, createdAt, replyId }: ReplyProps) => {
+  return (
+    <article id={`reply-${replyId}`} className="space-y-2">
+      <ReplyHeader writer={writer} createdAt={createdAt} replyId={replyId} />
+      <ReplyContent content={content} />
+      <ReplyThread replyId={replyId} />
+    </article>
+  );
+};
