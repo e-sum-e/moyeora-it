09a6302a4bfe35de68f990824659e312b5502998
HYEONJEONG LEE <hlee220@myseneca.ca>
2025-05-22 15:04:29 +0900

    refactor: 리뷰시 사용했던 코드들 삭제

--- src/lib/api.ts
+++
@@ -1,7 +0,0 @@
-export const groupFetch = async (size: number = 10, cursor: number = 0) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/groups?size=${size}&cursor=${cursor}`,
-  );
-  const data = await response.json();
-  return data;
-};
