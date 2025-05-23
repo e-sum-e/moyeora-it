72b8dff19afa0e94c3bc8da236c34f619ec5c144
kkukileon305 <kkukileon305@gmail.com>
2025-05-23 12:07:55 +0900

    comment: user store 주석 추가

--- src/stores/useAuthStore.ts
+++ src/stores/useAuthStore.ts
@@ -11,7 +11,8 @@ export type UserStore = {
 /**
  * Zustand를 사용해 사용자 정보를 전역으로 관리하는 훅
  * - user: 현재 로그인한 사용자 정보, 초기값(로그인 안된 상태)은 null입니다
- * - login: 로그인한 유저 정보를 저장합니다,
+ * - login: 로그인한 유저 정보를 저장합니다, 로그인보다는 프로필 설정에 가깝습니다.
+ * !!프로필 수정시 login 함수로 설정해주셔야 합니다!!
  * - logout: 현재 로그인 유저 정보를 null로 설정하여 지웁니다.
  */
 const useAuthStore = create<UserStore>()(
