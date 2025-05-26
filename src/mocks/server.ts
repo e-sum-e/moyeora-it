039a0aa1ce60dba5a4a1566bd901f816b64a7165
kkukileon305 <kkukileon305@gmail.com>
2025-05-26 11:28:08 +0900

    feat: msw 인증 모듈 분리

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -2,9 +2,11 @@ import { setupServer } from 'msw/node';
 import { groupsHandlers } from './handler/groups';
 import { notificationsHandlers } from './handler/notifications';
 import { followingsHandlers } from './handler/followings';
+import { authenticationsHandlers } from '@/mocks/handler/authentications';
 
 export const server = setupServer(
   ...groupsHandlers,
   ...notificationsHandlers,
   ...followingsHandlers,
+  ...authenticationsHandlers,
 );
