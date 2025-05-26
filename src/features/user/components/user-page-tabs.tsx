7c57fd74087751f14a061e287c7f8e03fa181f19
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-26 18:22:16 +0900

    fix: href 프로퍼티 값 수정

--- src/features/user/components/user-page-tabs.tsx
+++ src/features/user/components/user-page-tabs.tsx
@@ -1,12 +1,18 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+
 import Link from 'next/link';
 
 export const UserPageTabs = () => {
+  const { id } = useParams();
+
   return (
     <ul className="flex gap-4">
       {[
-        { label: '팔로잉', href: '/mypage/followings' },
-        { label: '팔로워', href: '/mypage/followers' },
-        { label: '모임', href: '/mypage/meetings' },
+        { label: '팔로잉', href: `/user/${id}/followings` },
+        { label: '팔로워', href: `/user/${id}/followers` },
+        { label: '모임', href: `/user/${id}/groups` },
       ].map(({ label, href }) => (
         <li key={label}>
           <Link className="text-xl" href={href}>
