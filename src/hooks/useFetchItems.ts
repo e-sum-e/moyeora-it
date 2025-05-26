a6d0c5f54411f52efb00486aa58d7ce659ecdec5
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-26 10:06:12 +0900

    refactor: queryFn 프로퍼티 값 수정

--- src/hooks/useFetchItems.ts
+++ src/hooks/useFetchItems.ts
@@ -10,9 +10,11 @@ export const useFetchItems = ({
 }) => {
   return useSuspenseInfiniteQuery({
     queryKey: ['items', url, queryParams ?? {}],
-    queryFn: queryParams
-      ? () => request.get(url, queryParams)
-      : () => request.get(url),
+    queryFn: ({ pageParam }) =>
+      request.get(url, {
+        ...queryParams,
+        cursor: pageParam,
+      }),
     initialPageParam: 0,
     getNextPageParam(lastPage) {
       return lastPage.hasNext ? lastPage.cursor : null;
