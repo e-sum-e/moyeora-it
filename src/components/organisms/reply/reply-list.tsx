6bb5cb8df7b5848fda2bba2d9735a95e8dbe8dfd
Lee Yuna <leeyuna7689@gmail.com>
2025-05-26 16:30:26 +0900

    feat: 댓글을 추가하면 새로 생긴 댓글 위치로 이동하는 기능 추가

--- src/components/organisms/reply/reply-list.tsx
+++ src/components/organisms/reply/reply-list.tsx
@@ -6,6 +6,7 @@ import { useFetchInView } from '@/hooks/useFetchInView';
 import { useFetchItems } from '@/hooks/useFetchItems';
 import { Reply } from '@/types';
 import { useParams } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
 
 export const ReplyList = () => {
   const { groupId } = useParams();
@@ -22,9 +23,34 @@ export const ReplyList = () => {
 
   const allReplies = data.pages.flatMap((page) => page.items);
 
+  const [newReplyId, setNewReplyId] = useState<number | null>(null);
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+
+  // 스크롤 이동
+  useEffect(() => {
+    if (!newReplyId) return;
+
+    const element = document.getElementById(`reply-${newReplyId}`);
+
+    if (element) {
+      element.scrollIntoView({ behavior: 'instant', block: 'center' });
+      setNewReplyId(null);
+    } else {
+      // 찾을 수 없으면 제일 아래로
+      bottomRef.current?.scrollIntoView({
+        behavior: 'instant',
+        block: 'start',
+      });
+    }
+  }, [data, newReplyId]);
+
+  const replyFormSuccessHandler = (id: number) => {
+    setNewReplyId(id);
+  };
+
   return (
     <section className="w-4/5 mx-auto flex flex-col gap-10">
-      <ReplyForm />
+      <ReplyForm onSuccess={replyFormSuccessHandler} />
       <div>
         <ul>
           {allReplies.map(({ replyId, writer, content, createdAt }) => (
@@ -37,6 +63,7 @@ export const ReplyList = () => {
             />
           ))}
         </ul>
+        <div ref={bottomRef} id="reply-list-bottom" />
         {hasNextPage && !isFetchingNextPage && (
           <div ref={ref} className="h-2 -translate-y-100 bg-transparent" />
         )}
