beb955e79cd2f2adb7451e903122211f5c8397c0
kkukileon305 <kkukileon305@gmail.com>
2025-05-27 17:44:49 +0900

    feat: 로그인 트리거시 인증 페이지 사이에서는 저장하지 않도록 변경!

--- src/features/auth/components/LoginTriggerManager.tsx
+++ src/features/auth/components/LoginTriggerManager.tsx
@@ -8,6 +8,16 @@ type TriggerManagerProps = {
 
 const LoginTriggerManager = ({ prevPathname }: TriggerManagerProps) => {
   useEffect(() => {
+    // 인증 페이지들 사이에서 이동하는 경우 저장하지 않도록 처리!!!!!!!!!!!!!!!
+    if (
+      prevPathname === '/login' ||
+      prevPathname === '/register' ||
+      prevPathname === '/find-email' ||
+      prevPathname === '/find-password'
+    ) {
+      return;
+    }
+
     localStorage.setItem('login-trigger-path', prevPathname);
   }, []);
 
