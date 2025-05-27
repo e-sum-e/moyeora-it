7656534b50a1ed7ffc63388d9b22d4746a8377c5
Lee Yuna <leeyuna7689@gmail.com>
2025-05-27 15:49:48 +0900

    fix: 불필요한 대댓글 요청 방지
    - useQuery를 ReplyThred에서 RereplyList로 옮겨 ReplyThread가 렌더링될 때마다 대댓글 데이터를 요청하던 문제 해결
    - 대댓글 추가 시 isOpen을 true로 만들어 RereplyList가 렌더링 되며 쿼리를 실행하도록 수정

--- src/components/organisms/reply/rereply-list.tsx
+++ src/components/organisms/reply/rereply-list.tsx
@@ -1,13 +1,57 @@
 'use client';
 
 import { ReplyContent } from '@/components/molecules/reply/reply-content';
+import { useFetchInView } from '@/hooks/useFetchInView';
+import { useFetchItems } from '@/hooks/useFetchItems';
 import { Reply } from '@/types';
+import { useParams } from 'next/navigation';
+import { useEffect, useRef } from 'react';
 
 type RereplyListProps = {
-  rereplies: (Reply & { parentId: number })[];
+  parentReplyId: number;
+  newReplyId: number | null;
+  setNewReplyId: (id: number | null) => void;
 };
 
-export const RereplyList = ({ rereplies }: RereplyListProps) => {
+export const RereplyList = ({
+  newReplyId,
+  parentReplyId,
+  setNewReplyId,
+}: RereplyListProps) => {
+  const bottomRef = useRef<HTMLDivElement | null>(null);
+  const { groupId } = useParams();
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useFetchItems<Reply & { parentId: number }>({
+      url: `/groups/${groupId}/replies/${parentReplyId}`,
+      queryParams: {
+        size: 10,
+      },
+    });
+
+  const { ref } = useFetchInView({
+    fetchNextPage,
+  });
+
+  // 스크롤 이동
+  useEffect(() => {
+    if (newReplyId) {
+      const element = document.getElementById(`reply-${newReplyId}`);
+
+      if (element) {
+        element.scrollIntoView({ behavior: 'instant', block: 'center' });
+        setNewReplyId(null);
+      } else {
+        // 찾을 수 없으면 제일 아래로
+        bottomRef.current?.scrollIntoView({
+          behavior: 'instant',
+          block: 'end',
+        });
+      }
+    }
+  }, [data, newReplyId, setNewReplyId]);
+
+  const rereplies = data.pages.flatMap((page) => page.items);
+
   return (
     <div>
       <ul className="flex flex-col gap-5">
@@ -17,6 +61,10 @@ export const RereplyList = ({ rereplies }: RereplyListProps) => {
           </li>
         ))}
       </ul>
+      <div ref={bottomRef} />
+      {hasNextPage && !isFetchingNextPage && (
+        <div ref={ref} className="h-2 -translate-y-50 bg-transparent" />
+      )}
     </div>
   );
 };
