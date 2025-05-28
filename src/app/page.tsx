e231c7f995c6466d2808358eceee494772a172f9
seomi <seomi.lee@gmail.com>
2025-05-28 15:54:45 +0900

    feat: mock 데이터 불러와서 페이지에 렌더하기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,46 @@
+'use client';
+
+import { useFetchItems } from '@/hooks/useFetchItems';
+import { Group } from '@/types';
+import { Skill } from '@/types/enums';
+import { getYearMonthDayWithDot } from '@/utils/dateUtils';
+import { getSkillBadge } from '@/utils/getSkillBadge';
+
 export default function Home() {
-  return <div></div>;
+  const { data } = useFetchItems<Group>({
+    url: '/api/groups',
+  });
+
+  const groups = data.pages.flatMap((page) => page.items);
+  return (
+    <div>
+      <div className="">
+        <ul>
+          {groups.map((item) => (
+            <li key={item.id} className="border-1 border-y-teal-500 rounded-md">
+              <div>
+                <h3>{item.title}</h3>
+              </div>
+              <div>
+                참가 현황 :{item.participants.length}/{item.maxParticipants}
+              </div>
+              <div>
+                <div>모집 종료 : {getYearMonthDayWithDot(item.deadline)}</div>
+                <div>모임 시작 : {getYearMonthDayWithDot(item.startDate)}</div>
+                <div>모임 종료 : {getYearMonthDayWithDot(item.endDate)}</div>
+              </div>
+              <div>
+                <div>
+                  사용 기술 :
+                  <div className="flex gap-2">
+                    {item.skills.map((skill) => getSkillBadge(Skill[skill]))}
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
