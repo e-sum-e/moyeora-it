f59797be02bd05028a8cbf5123a9f3457ce429b5
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-28 11:36:17 +0900

    feat: 유저 정보가 존재하지 않을 경우 분기 처리

--- src/features/user/components/current-user-profile.tsx
+++ src/features/user/components/current-user-profile.tsx
@@ -7,32 +7,36 @@ import { getSkill } from '@/types/enums';
 
 /**
  * 현재 로그인 한 유저의 프로필 컴포넌트
- * 
- * 현재 로그인 한 유저의 프로필을 보여준다.   
- * 
+ *
+ * 현재 로그인 한 유저의 프로필을 보여준다.
+ *
  * @returns 현재 로그인 한 유저의 프로필 컴포넌트
  */
 
 export const CurrentUserProfile = () => {
   const user = useAuthStore((state) => state.user);
 
+  if (!user) {
+    return <div>나의 정보를 불러오는 데 실패했어요. 다시 로그인 해주세요.</div>;
+  }
+
   return (
     <div>
       <Avatar
         className="size-36"
-        imageSrc={user!.profileImage ?? user!.email}
+        imageSrc={user.profileImage ?? user.email}
         fallback="테스트"
       />
       <div className="flex flex-col gap-y-1">
-        <span>{user!.nickname}</span>
-        <span>{user!.email}</span>
-        <span>{user!.position}</span>
+        <span>{user.nickname}</span>
+        <span>{user.email}</span>
+        <span>{user.position}</span>
         <div className="flex items-center gap-x-2">
-          <span>별점 : {user!.rate}</span>
+          <span>별점 : {user.rate}</span>
           <Badge text="뱃지" className="bg-emerald-50 text-emerald-500" />
         </div>
         <ul>
-          {user!.skills?.map((skill) => (
+          {user.skills?.map((skill) => (
             <li key={skill}>
               <Badge
                 text={getSkill(skill)}
