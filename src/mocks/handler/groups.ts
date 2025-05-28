e6b0d94c53c695e211bc726e1a2c6192c7d72467
seomi <seomi.lee@gmail.com>
2025-05-28 10:39:17 +0900

    fix: baseUrl 수정에 따라 group 관련 url 수정

--- src/mocks/handler/groups.ts
+++ src/mocks/handler/groups.ts
@@ -1,16 +1,19 @@
 import { http, HttpResponse } from 'msw';
 
 export const groupsHandlers = [
-  http.post('http://localhost:4000/api/groups/:groupId/join', async ({ params, request }) => {
-    const { groupId } = params;
-    const body = await request.json() as {
-      userId: string;
-      status: 'approve' | 'deny';
-    };
-    console.log(groupId, body);
+  http.post(
+    'http://localhost:4000/api/groups/:groupId/join',
+    async ({ params, request }) => {
+      const { groupId } = params;
+      const body = (await request.json()) as {
+        userId: string;
+        status: 'approve' | 'deny';
+      };
+      console.log(groupId, body);
 
-    return HttpResponse.json({}, { status: 200 });
-  }),
+      return HttpResponse.json({}, { status: 200 });
+    },
+  ),
 
   http.get('http://localhost:4000/api/groups', () => {
     return HttpResponse.json({
@@ -31,60 +34,57 @@ export const groupsHandlers = [
       ],
     });
   }),
-  http.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/group`, () => {
+  http.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/group`, () => {
     return HttpResponse.json({
       success: true,
     });
   }),
-  http.get(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/groups/:groupId`,
-    () => {
-      return HttpResponse.json({
-        title: '스터디1',
-        deadline: '2025-05-24',
-        startDate: '2025-05-20',
-        endDate: '2025-05-24',
-        maxParticipants: 10,
-        description: '스터디1 설명',
-        position: [1, 3],
-        skills: [1, 2],
-        createdAt: '2025-05-20',
-        type: 'study',
-        autoAllow: true,
-        host: {
+  http.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/groups/:groupId`, () => {
+    return HttpResponse.json({
+      title: '스터디1',
+      deadline: '2025-05-24',
+      startDate: '2025-05-20',
+      endDate: '2025-05-24',
+      maxParticipants: 10,
+      description: '스터디1 설명',
+      position: [1, 3],
+      skills: [1, 2],
+      createdAt: '2025-05-20',
+      type: 'study',
+      autoAllow: true,
+      host: {
+        id: 'abcd123',
+        name: '사용자1',
+        profileImage: 'https://github.com/shadcn.png',
+      },
+      isApplicant: false,
+      participants: [
+        {
           id: 'abcd123',
-          name: '사용자1',
+          name: '팀원1',
           profileImage: 'https://github.com/shadcn.png',
         },
-        isApplicant: false,
-        participants: [
-          {
-            id: 'abcd123',
-            name: '팀원1',
-            profileImage: 'https://github.com/shadcn.png',
-          },
-          {
-            id: 'abcd123',
-            name: '팀원1',
-            profileImage: 'https://github.com/shadcn.png',
-          },
-          {
-            id: 'abcd123',
-            name: '팀원1',
-            profileImage: 'https://github.com/shadcn.png',
-          },
-          {
-            id: 'abcd123',
-            name: '팀원1',
-            profileImage: 'https://github.com/shadcn.png',
-          },
-          {
-            id: 'abcd123',
-            name: '팀원1',
-            profileImage: 'https://github.com/shadcn.png',
-          },
-        ],
-      });
-    },
-  ),
+        {
+          id: 'abcd123',
+          name: '팀원1',
+          profileImage: 'https://github.com/shadcn.png',
+        },
+        {
+          id: 'abcd123',
+          name: '팀원1',
+          profileImage: 'https://github.com/shadcn.png',
+        },
+        {
+          id: 'abcd123',
+          name: '팀원1',
+          profileImage: 'https://github.com/shadcn.png',
+        },
+        {
+          id: 'abcd123',
+          name: '팀원1',
+          profileImage: 'https://github.com/shadcn.png',
+        },
+      ],
+    });
+  }),
 ];
