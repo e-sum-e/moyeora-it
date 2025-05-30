fea2a9158f40d1804706689e2bdf08eee2cd658e
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-30 15:30:59 +0900

    comment: 주석 수정

--- src/features/user/follow/hooks/useToggleFollow.ts
+++ src/features/user/follow/hooks/useToggleFollow.ts
@@ -6,7 +6,7 @@ import { useParams } from 'next/navigation';
 /**
  * 팔로우, 언팔로우 기능을 위한 커스텀 훅
  *
- * useId prop를 추가한 이유는 이 커스텀 훅을 호출한 버튼이 사용된 위치에 따라 무효화할 쿼리 키를 다르게 설정하기 위함
+ * usedIn prop를 추가한 이유는 이 커스텀 훅을 호출한 버튼이 사용된 위치에 따라 무효화할 쿼리 키를 다르게 설정하기 위함
  *
  * @param userId: 팔로우, 언팔로우 대상 유저 ID
  * @param isFollowing: 팔로우 상태
