6e8467101e60646f2fb5a1b7387218af0c2ea701
Lee Yuna <leeyuna7689@gmail.com>
2025-05-30 10:24:46 +0900

    refactor: DOMPurify 로직 함수로 분리하여 렌더링 로직 개선

--- src/components/atoms/group-description/index.tsx
+++ src/components/atoms/group-description/index.tsx
@@ -2,12 +2,11 @@
 
 import DOMPurify from 'dompurify';
 
+const sanitizeHTML = (markdown: string) => {
+  const renderedHTML = DOMPurify.sanitize(markdown);
+  return { __html: renderedHTML };
+};
+
 export const GroupDescription = ({ description }: { description: string }) => {
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: DOMPurify.sanitize(description),
-      }}
-    />
-  );
+  return <div dangerouslySetInnerHTML={sanitizeHTML(description)} />;
 };
