597030376424c560f5b04d0ed62e31c172431fb8
Kim Kangmin <rlarkdals1202@gmail.com>
2025-05-30 15:42:01 +0900

    fix: 사용하지 않는 변수 제거

--- src/mocks/handler/follow.ts
+++ src/mocks/handler/follow.ts
@@ -2,73 +2,78 @@ import { http, HttpResponse } from 'msw';
 import { User } from '@/types';
 
 export const followHandlers = [
-  http.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/followings`, ({ request, params }) => {
-    const userId = params.userId;
-    const url = new URL(request.url);
-    const searchParams = new URLSearchParams(url.search);
-    const search = searchParams.get('search');
-    console.log('in handler', search);
+  http.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/followings`,
+    ({ request }) => {
+      const url = new URL(request.url);
+      const searchParams = new URLSearchParams(url.search);
+      const search = searchParams.get('search');
+      console.log('in handler', search);
 
-    const items: User[] = Array.from({ length: 10 }).map((_, index) => {
-      return {
-        userId: String(index + 1),
-        email: `test${index + 1}@gmail.com`,
-        nickname: `테스트 닉네임${index + 1}`,
-        profileImage: `https://github.com/shadcn.png`,
-        position: 1,
-        skills: [1, 2, 3],
-        isFollowing: true,
-        isFollower: true,
-        rate: 4,
-      };
-    });
+      const items: User[] = Array.from({ length: 10 }).map((_, index) => {
+        return {
+          userId: String(index + 1),
+          email: `test${index + 1}@gmail.com`,
+          nickname: `테스트 닉네임${index + 1}`,
+          profileImage: `https://github.com/shadcn.png`,
+          position: 1,
+          skills: [1, 2, 3],
+          isFollowing: true,
+          isFollower: true,
+          rate: 4,
+        };
+      });
 
-    return HttpResponse.json({
-      hasNext: true,
-      cursor: 1,
-      items,
-    });
-  }),
+      return HttpResponse.json({
+        hasNext: true,
+        cursor: 1,
+        items,
+      });
+    },
+  ),
 
-  http.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/followers`, ({ request, params }) => {
-    const userId = params.userId;
-    const url = new URL(request.url);
-    const searchParams = new URLSearchParams(url.search);
-    const search = searchParams.get('search');
-    console.log('in handler', search);
+  http.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/followers`,
+    ({ request }) => {
+      const url = new URL(request.url);
+      const searchParams = new URLSearchParams(url.search);
+      const search = searchParams.get('search');
+      console.log('in handler', search);
 
-    const items: User[] = Array.from({ length: 10 }).map((_, index) => {
-      return {
-        userId: String(index + 1),
-        email: `test${index + 1}@gmail.com`,
-        nickname: `테스트 닉네임${index + 1}`,
-        profileImage: `https://github.com/shadcn.png`,
-        position: 1,
-        skills: [1, 2, 3],
-        isFollowing: true,
-        isFollower: true,
-        rate: 4,
-      };
-    });
+      const items: User[] = Array.from({ length: 10 }).map((_, index) => {
+        return {
+          userId: String(index + 1),
+          email: `test${index + 1}@gmail.com`,
+          nickname: `테스트 닉네임${index + 1}`,
+          profileImage: `https://github.com/shadcn.png`,
+          position: 1,
+          skills: [1, 2, 3],
+          isFollowing: true,
+          isFollower: true,
+          rate: 4,
+        };
+      });
 
-    return HttpResponse.json({
-      hasNext: true,
-      cursor: 1,
-      items,
-    });
-  }),
+      return HttpResponse.json({
+        hasNext: true,
+        cursor: 1,
+        items,
+      });
+    },
+  ),
 
-  http.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/follow`, ({ params }) => {
-    const userId = params.userId;
-    return HttpResponse.json({
-      message: '팔로우 성공',
-    });
-  }),
+  http.post(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/follow`,
+    () => {
+      return HttpResponse.json({
+        message: '팔로우 성공',
+      });
+    },
+  ),
 
   http.post(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/unfollow`,
-    ({ params }) => {
-      const userId = params.userId;
+    () => {
       return HttpResponse.json({
         message: '언팔로우 성공',
       });
@@ -77,8 +82,7 @@ export const followHandlers = [
 
   http.post(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/unfollower`,
-    ({ params }) => {
-      const userId = params.userId;
+    () => {
       return HttpResponse.json({
         message: '팔로워 삭제 성공',
       });
@@ -87,8 +91,7 @@ export const followHandlers = [
 
   http.get(
     `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/followers/count`,
-    ({ params }) => {
-      const userId = params.userId;
+    () => {
       return HttpResponse.json({
         count: 100,
       });
@@ -96,9 +99,8 @@ export const followHandlers = [
   ),
 
   http.get(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/followings/count`,
-    ({ params }) => {
-      const userId = params.userId;
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/users/:userId/followings/count`,
+    () => {
       return HttpResponse.json({
         count: 50,
       });
