7b7e29b33a4bcc577a4067a0a08b088bb1dce5d9
Lee Yuna <leeyuna7689@gmail.com>
2025-06-02 14:07:28 +0900

    feat: 사용자에 따라 모임 상세 페이지 버튼 렌더링

--- src/app/group/[groupId]/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default async function GroupPageLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <main className="w-4/5 mx-auto flex flex-col gap-10">{children}</main>;
-}
--- src/app/group/[groupId]/page.tsx
+++ src/app/group/[groupId]/page.tsx
@@ -1,5 +1,6 @@
 import { request } from '@/api/request';
 import { GroupDescription } from '@/components/atoms/group-description';
+import { GroupActionButtons } from '@/components/molecules/gorup-action-buttons';
 import { GroupDetaiilCard } from '@/components/organisms/group-detail-card';
 import { ParticipantListModal } from '@/components/organisms/participant-list-modal';
 import { ReplyList } from '@/components/organisms/reply/reply-list';
@@ -19,14 +20,19 @@ export default async function GroupDetailPage({
 }: GroupDetailPageProps) {
   const groupId = (await params).groupId;
   const data = (await request.get(`/groups/${groupId}`)) as GroupDetail;
-  const { participants, description } = data;
+  const { participants, description, host, isApplicant } = data;
 
   return (
     <div>
-      <GroupDetaiilCard info={data} />
-      <GroupDescription description={description} />
-      <ParticipantListModal participants={participants} />
-      <ReplyList />
+      <main className="w-4/5 mx-auto flex flex-col gap-10">
+        <GroupDetaiilCard info={data} />
+        <GroupDescription description={description} />
+        <ParticipantListModal participants={participants} />
+        <ReplyList />
+      </main>
+      <footer className="fixed bottom-0 z-50 bg-white border-t-2 py-2 px-5 w-full flex justify-end gap-3">
+        <GroupActionButtons hostId={host.userId} isApplicant={isApplicant} />
+      </footer>
     </div>
   );
 }
