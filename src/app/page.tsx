8dce4e0bc716879a9d23894e51fbe6171fe9be3f
seomi <seomi.lee@gmail.com>
2025-06-02 13:55:16 +0900

    feat: 서버 컴포넌트에서 초기 데이터 prefetch -> 무한 스크롤

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,49 @@
+import { request } from '@/api/request';
 import { GroupList } from '@/components/organisms/group';
+import { Position, Skill } from '@/types/enums';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 import { Suspense } from 'react';
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Record<string, string | undefined>;
+}) {
+  const awaitedSearchParams = await searchParams; // searchParams가 Promise 객체여서 await으로 벗겨내야 함
+  const queryClient = new QueryClient();
+  const queryParams = {
+    type: awaitedSearchParams.type ?? '',
+    skill: Skill[awaitedSearchParams.skill as keyof typeof Skill] ?? '',
+    position:
+      Position[awaitedSearchParams.position as keyof typeof Position] ?? '',
+    sort: awaitedSearchParams.sort ?? 'createdAt',
+    order: awaitedSearchParams.order ?? 'desc',
+    search: awaitedSearchParams.search ?? '',
+  };
+
+  // console.log('✅ Fetching data from server ', queryParams); // DEV: 💡 서버 컴포넌트에서 prefetch 하는지 확인용
+
+  await queryClient.fetchInfiniteQuery({
+    queryKey: ['items', '/groups', queryParams],
+    queryFn({ pageParam }) {
+      return request.get('/groups', {
+        ...queryParams,
+        size: 10,
+        cursor: pageParam,
+      });
+    },
+    initialPageParam: 0,
+  });
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <GroupList />
-    </Suspense>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <GroupList searchParams={awaitedSearchParams} />
+      </Suspense>
+    </HydrationBoundary>
   );
 }
