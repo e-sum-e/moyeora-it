c6190a3f645628d55b278785a3cbc3e62a7f8ae6
HYEONJEONG LEE <hlee220@myseneca.ca>
2025-06-03 16:38:00 +0900

    feat: ci-cd test

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'moyeora-it-FE',
+      script: 'yarn',
+      args: 'start',
+      interpreter: '/bin/bash',
+      cwd: '/projects/moyeora-it-FE', 
+      env: {
+        NODE_ENV: 'development',
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+}
