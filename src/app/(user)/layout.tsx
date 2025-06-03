aa72a275249c0c83980d3b5e6c7860bd3116527f
kkukileon305 <kkukileon305@gmail.com>
2025-06-03 11:32:59 +0900

    feat: 클라이이언트 가드 쿠키 검사로 변경

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,9 +1,9 @@
+import checkAuthCookie from '@/features/auth/utils/checkAuthCookie';
+import { redirect } from 'next/navigation';
 import { UserProfile } from '@/features/user/components/user-profile';
 import { UserPageTabs } from '@/features/user/components/user-page-tabs';
-import ClientAuthGuard from '@/features/auth/components/ClientAuthGuard';
-// import checkAuthCookie from '@/features/auth/utils/checkAuthCookie';
 
-export default function UserPageLayout({
+export default async function UserPageLayout({
   children,
 }: {
   children: React.ReactNode;
@@ -11,7 +11,11 @@ export default function UserPageLayout({
   // ISSUE1: 쿠키가 있다고 인증 처리를 하면 안됨 -> 쿠키는 유저가 임의로 조작가능, api 통신필요
   // ISSUE2: 근데 로그인 안햇다고 해당 페이지 안보여주나요? 일단 가드처리해뒀어요
 
-  // const hasToken = await checkAuthCookie();
+  const isValid = await checkAuthCookie();
+
+  if (!isValid) {
+    redirect('/login');
+  }
 
   // if (!hasToken) {
   //   redirect('/login');
@@ -19,11 +23,9 @@ export default function UserPageLayout({
 
   return (
     <>
-      <ClientAuthGuard>
-        <UserProfile />
-        <UserPageTabs />
-        <main>{children}</main>
-      </ClientAuthGuard>
+      <UserProfile />
+      <UserPageTabs />
+      <main>{children}</main>
     </>
   );
 }
