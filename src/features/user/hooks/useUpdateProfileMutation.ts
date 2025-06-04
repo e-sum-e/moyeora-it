36ab47f3bf0c45ec5eb8e6a4a0d06f123f2387be
Kim Kangmin <rlarkdals1202@gmail.com>
2025-06-04 10:23:11 +0900

    fix: 프로필 수정 안 되는 에러 수정

--- src/features/user/hooks/useUpdateProfileMutation.ts
+++ src/features/user/hooks/useUpdateProfileMutation.ts
@@ -1,6 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 import useAuthStore from '@/stores/useAuthStore';
 import { Position, Skill } from '@/types/enums';
+import { getPosition, getSkill } from '@/types/enums';
 import { toast } from 'sonner';
 import { User } from '@/types';
 import { request } from '@/api/request';
@@ -23,22 +24,21 @@ export const useUpdateProfileMutation = () => {
     }) => {
       const formData = new FormData();
       formData.append('nickname', data.nickname);
-      formData.append('position', JSON.stringify(data.position));
-      formData.append('skills', JSON.stringify(data.skills));
+      formData.append('position', getPosition(data.position));
+      formData.append('skills', data.skills.map(getSkill).join(','));
       if (data.file) {
-        formData.append('file', data.file);
+        formData.append('image', data.file);
       }
 
-      //ISSUE: 500에러 날라와요
-      return request.patch(`/v1/user/edit`,
-        {
-          'Content-Type': 'multipart/form-data',
-        },
-        formData,
-        {
-          credentials: 'include',
-        },
-      ).then((res) => res.json());
+      return request
+        .patch(
+          `/v1/user/edit`,
+          {},
+          formData,
+          {
+            credentials: 'include',
+          },
+        );
     },
     onSuccess(data: User) {
       setUser(data);
