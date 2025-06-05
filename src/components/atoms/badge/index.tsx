368af94135b22716fa998959153dc899e4610601
seomi <seomi.lee@gmail.com>
2025-06-05 18:03:24 +0900

    style: Badge에서 사용하는 태그 변경(div -> span)

--- src/components/atoms/badge/index.tsx
+++ src/components/atoms/badge/index.tsx
@@ -3,5 +3,5 @@ type BadgeProps = {
   className?: string;
 };
 export const Badge = ({ text, className }: BadgeProps) => {
-  return <div className={`p-1 rounded-sm ${className}`}>{text}</div>;
+  return <span className={`p-1 rounded-sm ${className}`}>{text}</span>;
 };
