6b27d8da695ea1cd6ad4db6f63af790f5703084c
kkukileon305 <kkukileon305@gmail.com>
2025-06-05 13:07:25 +0900

    refactor: 불필요한 로직 제거

--- src/app/(auth)/find-password/page.tsx
+++ src/app/(auth)/find-password/page.tsx
@@ -2,15 +2,14 @@
 
 import Link from 'next/link';
 import FindPassword from '@/components/organisms/find-password-form';
-import ClientAuthGuard from '@/features/auth/components/ClientAuthGuard';
 
 export default function Page() {
   return (
-    <ClientAuthGuard isNeedUser={false}>
+    <>
       <div>
         <FindPassword />
         <Link href="/login">로그인으로 돌아가기</Link>
       </div>
-    </ClientAuthGuard>
+    </>
   );
 }
--- src/features/auth/components/ClientAuthGuard.tsx
+++ src/features/auth/components/ClientAuthGuard.tsx
@@ -6,32 +6,18 @@ import useAuthStore from '@/stores/useAuthStore';
 
 type ClientAuthGuardProps = {
   children: React.ReactNode;
-  isNeedUser: boolean;
 };
 
-export default function ClientAuthGuard({
-  children,
-  isNeedUser = true,
-}: ClientAuthGuardProps) {
+export default function ClientAuthGuard({ children }: ClientAuthGuardProps) {
   const user = useAuthStore((state) => state.user);
   const router = useRouter();
 
   useEffect(() => {
-    if (!isNeedUser) {
-      if (user) {
-        router.replace('/');
-      }
-      return;
-    }
-
     if (!user) {
       router.replace('/login');
     }
     // eslint-disable-next-line
   }, [user]);
-
-  if (!isNeedUser && user) return null;
-  if (isNeedUser && !user) return null;
 
   return <>{children}</>;
 }
