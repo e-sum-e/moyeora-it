11bc826702e3896b4309528201601ec0baa56ad5
Kim Kangmin <rlarkdals1202@gmail.com>
2025-06-07 12:26:49 +0900

    fix: 매개변수 객체 타입, 요청 URL 수정 및 credentials mode include로 설정

--- src/features/user/follow/hooks/useToggleFollow.ts
+++ src/features/user/follow/hooks/useToggleFollow.ts
@@ -8,7 +8,7 @@ import { useParams } from 'next/navigation';
  *
  * usedIn prop를 추가한 이유는 이 커스텀 훅을 호출한 버튼이 사용된 위치에 따라 무효화할 쿼리 키를 다르게 설정하기 위함
  *
- * @param userId: 팔로우, 언팔로우 대상 유저 ID
+ * @param userId: 현재 로그인한 유저 ID
  * @param isFollowing: 팔로우 상태
  * @param usedIn: 해당 커스텀 훅을 호출한 버튼이 사용된 위치 (프로필, 팔로잉 목록, 팔로워 목록)
  * @returns useMutation 반환값
@@ -18,7 +18,7 @@ export const useToggleFollow = ({
   isFollowing,
   usedIn,
 }: {
-  userId: string;
+  userId?: string;
   isFollowing: boolean;
   usedIn: string;
 }) => {
@@ -28,11 +28,12 @@ export const useToggleFollow = ({
   return useMutation({
     mutationFn() {
       return request.post(
-        `/v1/follow/${userId}/${isFollowing ? 'unfollow' : ''}`,
+        `/v1/follow/${userId ?? id}/${isFollowing ? 'unfollow' : ''}`,
         {
           'Content-Type': 'application/json',
         },
-        JSON.stringify({ userId }),
+        JSON.stringify({ id: userId ?? id }),
+        { credentials: 'include' },
       );
     },
     onError() {
