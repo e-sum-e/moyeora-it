5b0849516c0dbfb680d126681e3e6e0801127fa7
seomi <seomi.lee@gmail.com>
2025-06-09 14:42:46 +0900

    style: shadcn button 스타일 되돌림(primary 적용에 따라 자연스럽게 스타일 입혀짐)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,7 +8,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-white text-gray-800 shadow-xs hover:border-gray-400',
+        default:
+          'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
@@ -21,8 +22,8 @@ const buttonVariants = cva(
       },
       size: {
         default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 px-6 has-[>svg]:px-4',
+        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
       },
     },
@@ -48,11 +49,7 @@ function Button({
   return (
     <Comp
       data-slot="button"
-      className={cn(
-        'border-1',
-        'rounded-[8px]',
-        buttonVariants({ variant, size, className }),
-      )}
+      className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
   );
