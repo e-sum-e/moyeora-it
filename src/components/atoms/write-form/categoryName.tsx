2856de6712f237f584c2057fdbe4d53a7a29dedd
seomi <seomi.lee@gmail.com>
2025-06-09 17:27:28 +0900

    style: 여백 수정

--- src/components/atoms/write-form/categoryName.tsx
+++ src/components/atoms/write-form/categoryName.tsx
@@ -12,12 +12,11 @@ export const CategoryName = ({
   return (
     <div className="flex items-center mb-9 p-2 leading-10 border-b-4 border-gray-100">
       {number && (
-        <div className="flex w-5 h-5 justify-center items-center mr-1 text-white bg-primary rounded-full ">
+        <div className="flex w-5 h-5 justify-center items-center text-white bg-primary rounded-full">
           {number}
         </div>
       )}
-
-      <div className={`inline-block font-bold ${className}`}>{text}</div>
+      <div className={`ml-2 font-bold ${className}`}>{text}</div>
     </div>
   );
 };
