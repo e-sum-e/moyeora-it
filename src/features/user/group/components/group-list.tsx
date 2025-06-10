db3642db44df4fe9622d8d550ec7ad5cbad29ebd
Kim Kangmin <rlarkdals1202@gmail.com>
2025-06-10 10:24:28 +0900

    fix: 필수 쿼리 파라미터 전달
    - size가 필수 쿼리 파리미터이므로 추가 전달

--- src/features/user/group/components/group-list.tsx
+++ src/features/user/group/components/group-list.tsx
@@ -9,14 +9,16 @@ import flattenPages from '@/utils/flattenPages';
 
 /**
  * 그룹 목록 컴포넌트
- * 
+ *
  * 사용자가 참여/모집 중인 그룹 목록을 보여준다.
- * 
+ *
  * @returns 그룹 목록 컴포넌트
  */
 export const GroupList = () => {
-  const searchParams = useSearchParams()
-  const { search, type, status, sort } = Object.fromEntries(searchParams.entries());
+  const searchParams = useSearchParams();
+  const { search, type, status, sort } = Object.fromEntries(
+    searchParams.entries(),
+  );
 
   const { data, fetchNextPage, hasNextPage, isLoading } = useFetchItems<Group>({
     url: '/v2/groups/mygroup',
@@ -25,6 +27,7 @@ export const GroupList = () => {
       status: status ?? 'RECRUITING',
       sort: sort ?? 'deadline',
       ...(search && { search }),
+      size: 10,
     },
     options: {
       staleTime: 0,
@@ -41,13 +44,25 @@ export const GroupList = () => {
   const groupList = flattenPages<Group>(data.pages);
 
   return (
-    <ul>
-      {groupList.map((group) => (
-        <li key={group.id}>
-          <GroupCard item={group} />
-        </li>
-      ))}
-      {hasNextPage && <div ref={ref} />}
-    </ul>
+    <>
+      {groupList.length === 0 ? (
+        <div className='flex flex-col items-center justify-center mt-30'>
+          <p className='text-center font-medium text-gray-500'>
+            {status === 'RECRUITING'
+              ? <>모집 중인 그룹이 없어요. <br /> 새로운 그룹을 만들어보세요!</>
+              : <>참여 중인 그룹이 없어요. <br /> 새로운 그룹을 찾아보세요!</>}
+          </p>
+        </div>
+      ) : (
+        <ul>
+          {groupList.map((group) => (
+            <li key={group.id}>
+              <GroupCard item={group} />
+            </li>
+          ))}
+          {hasNextPage && <div ref={ref} />}
+        </ul>
+      )}
+    </>
   );
 };
