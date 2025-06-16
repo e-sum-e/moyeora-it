c347d4482758bb817d9c0bd7ba8532e894c22ec8
Lee Yuna <leeyuna7689@gmail.com>
2025-06-16 13:34:28 +0900

    refactor: 인증 쿠키 키를 환경 변수로 처리하도록 변경

--- src/utils/cookie.ts
+++ src/utils/cookie.ts
@@ -1,13 +1,16 @@
 import { cookies } from 'next/headers';
 
 export const getAuthCookieHeader = async () => {
+  const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN ?? 'accessToken';
+  const REFRESH_TOKEN_KEY = process.env.REFRESH_TOKEN ?? 'refreshToken';
+
   const cookieStore = await cookies();
-  const accessToken = cookieStore.get('accessToken');
-  const refreshToken = cookieStore.get('refreshToken');
+  const accessToken = cookieStore.get(ACCESS_TOKEN_KEY);
+  const refreshToken = cookieStore.get(REFRESH_TOKEN_KEY);
 
   return [
-    accessToken && `${accessToken.name}=${accessToken.value}`,
-    refreshToken && `${refreshToken.name}=${refreshToken.value}`,
+    accessToken && `${ACCESS_TOKEN_KEY}=${accessToken.value}`,
+    refreshToken && `${REFRESH_TOKEN_KEY}=${refreshToken.value}`,
   ]
     .filter(Boolean)
     .join('; ');
