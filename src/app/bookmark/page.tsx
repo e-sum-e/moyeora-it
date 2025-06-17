82026bd6a39d4bf606cf91df31962fe8270e0b97
HYEONJEONG LEE <hlee220@myseneca.ca>
2025-06-17 15:07:20 +0900

    design: 페이지 스켈레톤 삭제

--- src/app/bookmark/page.tsx
+++ src/app/bookmark/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next';
-import { BookmarkPageClient, CardSkeleton } from './BookmarkPageClient';
-import { Suspense } from 'react';
+import { BookmarkPageClient } from './BookmarkPageClient';
 
 
 export const metadata: Metadata = {
@@ -14,34 +13,8 @@ export const metadata: Metadata = {
     ],
   },
 }
-const SkeletonUI = () => {
-  return (
-    <div>
-      <section className="flex flex-row gap-4 mb-8 w-full">
-    <div className="w-[50px] h-[50px] bg-gray-200 rounded-xl animate-pulse" />
-    <div className="flex flex-col gap-2 justify-center flex-1">
-      <div className="h-6 w-32 bg-gray-200 rounded animate-pulse mb-1" />
-      <div className="h-4 w-48 bg-gray-100 rounded animate-pulse" />
-    </div>
-  </section>
-  <main>
-    <div className="flex gap-2 mb-6">
-      {[1, 2, 3].map((i) => (
-        <div
-          key={i}
-          className="h-9 w-24 bg-gray-200 rounded-full animate-pulse"
-        />
-      ))}
-    </div>
-    <CardSkeleton />
-  </main>
-    </div>
-  );
-};
 export default function BookmarkPage() {
   return (
-    <Suspense fallback={<SkeletonUI />}>
       <BookmarkPageClient />
-    </Suspense>
   );
 }
