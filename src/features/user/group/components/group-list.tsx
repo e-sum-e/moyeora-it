659232964d2288330738c0ed7f463f7ac2459edb
Kim Kangmin <rlarkdals1202@gmail.com>
2025-06-17 11:23:19 +0900

    feat: 종료된 모임 필터링 기능 추가 및 모임이 존재하지 않을 시의 메시지 추가

--- src/features/user/group/components/group-list.tsx
+++ src/features/user/group/components/group-list.tsx
@@ -1,19 +1,17 @@
 'use client';
 
-import { useBookmarkItems } from '@/hooks/useBookmarkItems';
 import { useFetchInView } from '@/hooks/useFetchInView';
 import { useFetchItems } from '@/hooks/useFetchItems';
 import { Group } from '@/types';
-import { useParams } from 'next/navigation';
+import { useParams, useSearchParams } from 'next/navigation';
 import flattenPages from '@/utils/flattenPages';
 import useAuthStore from '@/stores/useAuthStore';
 import { GroupListItem } from '@/features/user/group/components/group-list-item';
+import { isBeforeToday } from '@/utils/dateUtils';
 
 type GroupListProps = {
-  status: 'PARTICIPATING' | 'RECRUITING';
+  status: 'PARTICIPATING' | 'RECRUITING' | 'ENDED';
 }
-import { useParams, useSearchParams } from 'next/navigation';
-import { useEffect } from 'react';
 
 /**
  * 모임 목록 컴포넌트
@@ -23,8 +21,6 @@ import { useEffect } from 'react';
  * @returns 모임 목록 컴포넌트
  */
 export const GroupList = ({ status }: GroupListProps) => {
-  console.log(status);
-
   const user = useAuthStore((state) => state.user);
 
   const { id } = useParams();
@@ -40,7 +36,7 @@ export const GroupList = ({ status }: GroupListProps) => {
     queryParams: {
       type: type ?? 'study',
       status: 'PARTICIPATING',
-      size: 10,
+      size: status === 'ENDED' ? 50 : 10,
       ...(search && { search }),
     },
     options: {
@@ -57,14 +53,19 @@ export const GroupList = ({ status }: GroupListProps) => {
   });
 
   // @ts-expect-error 객체 안에 또 group 프로퍼티가 존재함.
-  const groupList = flattenPages<Group>(data.pages).map((e) => e.group);
+  let groupList = flattenPages<Group>(data.pages).map((e) => e.group);
+  if (status === 'ENDED') {
+    groupList = groupList.filter((group) => isBeforeToday(group.endDate));
+  }
 
   return (
     <>
       {groupList.length === 0 ? (
         <div className="flex flex-col items-center justify-center mt-30">
           <p className="text-center font-medium text-gray-500">
-            모집 중인 그룹이 없어요.
+            {status === 'RECRUITING' && '모집 중인 모임이 없어요.'}
+            {status === 'PARTICIPATING' && '참여 중인 모임이 없어요.'}
+            {status === 'ENDED' && '종료된 모임이 없어요.'}
           </p>
         </div>
       ) : (
