5d84cdfc179eebaf1af61401d6fdffddcb8228bc
seomi <seomi.lee@gmail.com>
2025-06-18 16:54:24 +0900

    design: 원안에 숫자가 들어간 `circle-number` 원안에 i가 표기된 `circle-info` 컴포넌트 생성

---
+++ src/components/atoms/circle-icon/circle-info.tsx
@@ -0,0 +1,11 @@
+type CircleInfoProps = { className?: string };
+
+export const CircleInfo = ({ className }: CircleInfoProps) => {
+  return (
+    <div
+      className={`flex w-4 h-4 justify-center items-center text-xs text-white bg-gray-600 rounded-full ${className}`}
+    >
+      <span>i</span>
+    </div>
+  );
+};
