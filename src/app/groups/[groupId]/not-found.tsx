1422a07a450d7d0d73ce2d5dba2116e70f4b87cb
Lee Yuna <leeyuna7689@gmail.com>
2025-06-19 09:29:24 +0900

    design: 모임 상세 페이지 Not Found 페이지에 Empty 컴포넌트 적용

--- src/app/groups/[groupId]/not-found.tsx
+++ src/app/groups/[groupId]/not-found.tsx
@@ -1,7 +1,9 @@
+import { Empty } from '@/components/organisms/empty';
+
 export default function GroupDetailNotFoundPAge() {
   return (
-    <div className="text-center mt-20">
-      <h1>삭제 되었거나 존재하지 않는 모임입니다.</h1>
+    <div className="text-center mt-40">
+      <Empty mainText="삭제 되었거나 존재하지 않는 모임입니다." subText="" />
     </div>
   );
 }
