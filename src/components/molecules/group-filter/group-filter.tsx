b19e21522ec205f5c05d7aaa67988687856bd9fc
Kim Kangmin <rlarkdals1202@gmail.com>
2025-06-20 15:38:34 +0900

    feat: 정렬 Select 추가

--- src/components/molecules/group-filter/group-filter.tsx
+++ src/components/molecules/group-filter/group-filter.tsx
@@ -1,8 +1,16 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { SearchInput } from '@/components/molecules/search-input/search-input';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { SelectGroup } from '@radix-ui/react-select';
 
 /**
  * 모임 목록 필터 컴포넌트
@@ -12,11 +20,24 @@ import { SearchInput } from '@/components/molecules/search-input/search-input';
  * @returns 모임 목록 필터 컴포넌트
  */
 export const GroupFilter = () => {
+  const router = useRouter();
+
   const pathname = usePathname();
 
   const searchParams = useSearchParams();
 
   const type = searchParams.get('type') ?? 'study';
+
+  const order = searchParams.get('order') ?? 'latest';
+
+  const handleOrderSelectChange = (value: string) => {
+    const queryParams = new URLSearchParams(searchParams);
+    if (queryParams.has('order') && queryParams.get('order') === value) {
+      return;
+    }
+    queryParams.set('order', value);
+    router.push(`${pathname}?${queryParams.toString()}`);
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -46,6 +67,24 @@ export const GroupFilter = () => {
           containerClassName="w-[150px] md:w-[200px]"
           placeholder="검색"
         />
+      </div>
+      <div className="flex">
+        <Select
+          defaultValue={order ?? 'latest'}
+          onValueChange={handleOrderSelectChange}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="최신순" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem defaultChecked value="latest">
+                최신순
+              </SelectItem>
+              <SelectItem value="oldest">오래된 순</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
     </div>
   );
